ce60797c16bcc9829abd002983a01c1e274a2ce9
Deepkumar Patel <dspguru97@gmail.com>
2021-03-12 11:01:15 -0500

    changeUiAbit

--- public/scripts/components/TheMusicThumbnailComp.js
+++ public/scripts/components/TheMusicThumbnailComp.js
@@ -2,12 +2,10 @@ export default{
     name: "TheMusicThumbnail",
     props: ['music'],
     template:`
-        <div class="music-container">
-                <div class="music-thumb">
-                    <img :src='"images/music/" + music.thumbnail' alt="music thumb">
-                    <p>{{music.song_name}}</p>
-                    <p>{{music.singer}}</p>
-                </div>
+        <div class="song-box">
+            <img :src='"/images/music/" + music.thumbnail' alt="music thumb">
+            <p>{{music.song_name}}</p>
+            <p>-By {{music.singer}}</p>
         </div>
     `
 }
